7f8cef4cf54caa112212c923e20fd1b80937608e
Walid Bekkal <walidsdt.2@gmail.com>
2022-12-05 15:35:17 +0100

    switched to python parsing much easier, parsing done, working of soliving the equation

--- computorv1.cpp
+++
@@ -1,9 +0,0 @@
-#include "computorv1.hpp"
-int main(int argc, char **argv)
-{
-	if (argc != 2)
-		return (std::cout << "Wrong usage : ./computorv1 \"equation\"" << std::endl, 1);
-	Equation eq(argv[1]);
-	eq.parseEquation();
-	return 0;
-}
---
+++ computorv1.py
@@ -0,0 +1,60 @@
+# equation invalid format : - 5 * X^0 + 5 * X^1 - 4 * X^2 + 3 * X^3 = 30 * X^0
+# equation valid format : - 5 * X^0 + 5 * X^1 - 4 * X^2 = 30 * X^0
+import re
+# parse equation from user input
+def get_equation() : 
+	equation = input("Enter equation: ")
+	if (equation.count("=") != 1) :
+		print("Invalid equation")
+		exit()
+	if (equation[:1] != '-') :
+		equation = "+ " + equation
+	equation = equation.replace(" ", "")
+	equation = equation.replace("=", "-(") + ")"
+	print(equation)
+	return equation
+# regex getter to get coefficients and signs in the equation
+def get_coefficients() :
+	equation = get_equation()
+	coefficients = []
+	signs = []
+	coefficients = re.findall(r"[-+]?\d*\.\d+|\d+", equation)
+	signs = re.findall(r"[-+]", equation)
+	return coefficients, signs
+# detect if equation is degree 3 or more
+def find_invalid_degree(coefficients) :
+	invalid_degree = 3
+	for i in range(len(coefficients)) :
+		if (i % 2 != 0) :
+			if (int(coefficients[i]) >= invalid_degree) :
+				print("The polynomial degree is stricly greater than 2, I can't solve.")
+				exit()
+# attribute signs to coefficients (because it was not done in parsing)
+def signs_to_coefficients(coefficients, signs) :
+	coefficients = [float(i) for i in coefficients]
+	for i in range(len(coefficients)) :
+		if (i % 2 == 0) :
+			if (signs[i // 2] == "-") :
+				coefficients[i] = -coefficients[i]
+	return coefficients
+# simplify equation
+def simplify_equation(coefficients, signs) :
+	simplified_coefficients = [0] * len(signs)
+	print(simplified_coefficients)
+	print(coefficients)
+	for i in range(len(coefficients)) :
+		if (i % 2 == 0) :
+			simplified_coefficients[int(coefficients[i + 1])] += coefficients[i]
+	simplified_equation = ""
+	for i in range(len(simplified_coefficients)) :
+		if (simplified_coefficients[i] != 0) :
+			if (simplified_coefficients[i] > 0) :
+				simplified_equation += "+ "
+			simplified_equation += str(simplified_coefficients[i]) + " * X^" + str(i) + " "
+	print("Reduced form: " + simplified_equation + "= 0")
+	return simplified_coefficients, simplified_equation
+if __name__ == "__main__":
+	coefficients, signs = get_coefficients()
+	find_invalid_degree(coefficients)
+	coefficients = signs_to_coefficients(coefficients, signs)
+	simplified_coefficients, simplified_equation = simplify_equation(coefficients, signs)
